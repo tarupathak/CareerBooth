0285ef494c475381325b5699dada57f5be363df1
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-08-27 20:38:33 +0530

    Added all the files

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -8,36 +8,42 @@ import {
   FlatList,
 } from "react-native";
 import { useRouter } from "expo-router";
+
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
-const Welcome = () => {
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Taru</Text>
-        <Text style={styles.userName}>Find your perfect job</Text>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
+
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            onChange={() => {}}
-            placeholder="What are you looking for?"
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder='What are you looking for?'
           />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
-            resizeMode="contain"
+            resizeMode='contain'
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
       </View>
+
       <View style={styles.tabsContainer}>
         <FlatList
           data={jobTypes}
@@ -52,8 +58,8 @@ const Welcome = () => {
               <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
-          keyExtractor={item => item}
-          contentContainerStyle ={{columnGap: SIZES.small}}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
       </View>
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,34 +1,32 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-
-const options = {
-  method: 'GET',
-  url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-  headers: {
-    'X-RapidAPI-Key': '8fb760297emsh744cd54ddf77004p138037jsn92d8a6eef12b',
-    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-  },
-  params: {
-    ...query
-  },
-};
-
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
 
   const fetchData = async () => {
     setIsLoading(true);
+
     try {
       const response = await axios.request(options);
+
       setData(response.data.data);
+      setIsLoading(false);
     } catch (error) {
       setError(error);
-      alert("There is an error!");
+      console.log(error)
     } finally {
       setIsLoading(false);
     }
